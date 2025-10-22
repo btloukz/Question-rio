e16d4136a2ab067fe82191397476d11a2618ba58
btloukz <2024010409@aluno.restinga.ifrs.edu.br>
2025-10-22 17:28:31 -0300

    Create App component to render Questionario

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Questionario from './Questionario'
+import Lista from './lista'
+
+function App() {
+
+
+  return (
+    <>
+      <Questionario lista={Lista}></Questionario>
+    </>
+  )
+}
+
+export default App
